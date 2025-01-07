d52fa5285adb5977006ccd01d95bab0b6561b2e7
Piotr Resztak <piotr.resztak@futurfusion.io>
2025-01-07 10:53:03 +0000

    Fix issue with listing instances when cluster member is down
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>
    License: GPL-3.0-only (without Canonical CLA)
    (cherry picked from commit 1fea197db8290ba634c6d461bea598964f6643cd)

--- src/util/instances.tsx
+++ src/util/instances.tsx
@@ -72,7 +72,7 @@ export const getInstanceType = (instance: LxdInstance) => {
     (item) => item.value === instance.type,
   )?.label;
 
-  if (instance.config["volatile.container.oci"] === "true") {
+  if (instance.config?.["volatile.container.oci"] === "true") {
     return `${label} (App)`;
   }
 
