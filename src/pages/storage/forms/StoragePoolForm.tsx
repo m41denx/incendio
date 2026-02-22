81b9cd4d9863e619fbaaa74dcb4afa3c4489a9a2
Piotr Resztak <piotr.resztak@futurfusion.io>
2026-02-22 18:01:04 +0100

    Fix passing source for btrfs driver
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>
    License: GPL-3.0-only (without Canonical CLA)
    (cherry picked from commit e6036709915116f6afd70ffe7f59a5cb5eb686d3)

--- src/pages/storage/forms/StoragePoolForm.tsx
+++ src/pages/storage/forms/StoragePoolForm.tsx
@@ -142,9 +142,7 @@ export const toStoragePool = (
     config: {
       ...missingConfigFields,
       ...getConfig(),
-      source: hasSource(values.driver, hasRemoteDropSource)
-        ? values.source
-        : undefined,
+      source: values.source,
     },
   };
 };
