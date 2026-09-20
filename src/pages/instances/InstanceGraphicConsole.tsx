stgraber/incus-ui-canonical

Add console prompt to disconnect an existing session
Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>
License: GPL-3.0-only (without Canonical CLA)

src/pages/instances/InstanceGraphicConsole.tsx
@@ -27,12 +27,14 @@ interface SpiceWheelEvent extends CustomEvent {
 
 interface Props {
   instance: LxdInstance;
+  force: boolean;
   onMount: (handler: () => void) => void;
   onFailure: (title: string, e: unknown, message?: string) => void;
 }
 
 const InstanceGraphicConsole: FC<Props> = ({
   instance,
+  force,
   onMount,
   onFailure,
 }) => {
@@ -64,7 +66,7 @@ const InstanceGraphicConsole: FC<Props> = ({
     SpiceHtml5.handle_resize();
   };
 
-  const openVgaConsole = async () => {
+  const openVgaConsole = async (force: string) => {
     if (!name) {
       onFailure("Missing name", new Error());
       return;
@@ -75,7 +77,7 @@ const InstanceGraphicConsole: FC<Props> = ({
     }
 
     setVgaLoading(true);
-    const result = await connectInstanceVga(name, project).catch((e) => {
+    const result = await connectInstanceVga(name, project, force).catch((e) => {
       setVgaLoading(false);
       if (isRunning) {
         onFailure("Connection failed", e);
@@ -145,7 +147,7 @@ const InstanceGraphicConsole: FC<Props> = ({
 
   useEffect(() => {
     notify.clear();
-    const websocketPromise = openVgaConsole();
+    const websocketPromise = openVgaConsole(force);
     return () => {
       try {
         window.spice_connection?.stop();

m41denx/incendio

(cherry picked from commit fb5b8085575655dfbed885ca15c10ef4c74b91fe)

src/pages/instances/InstanceGraphicConsole.tsx
@@ -25,12 +25,14 @@ interface SpiceWheelEvent extends CustomEvent {
 
 interface Props {
   instance: LxdInstance;
+  force: boolean;
   onMount: (handler: () => void) => void;
   onFailure: (title: string, e: unknown, message?: string) => void;
 }
 
 const InstanceGraphicConsole: FC<Props> = ({
   instance,
+  force,
   onMount,
   onFailure,
 }) => {
@@ -62,7 +64,7 @@ const InstanceGraphicConsole: FC<Props> = ({
     SpiceHtml5.handle_resize();
   };
 
-  const openVgaConsole = async () => {
+  const openVgaConsole = async (force: boolean) => {
     if (!name) {
       onFailure("Missing name", new Error());
       return;
@@ -73,7 +75,7 @@ const InstanceGraphicConsole: FC<Props> = ({
     }
 
     setVgaLoading(true);
-    const result = await connectInstanceVga(name, project).catch((e) => {
+    const result = await connectInstanceVga(name, project, force).catch((e) => {
       setVgaLoading(false);
       if (isRunning) {
         onFailure("Connection failed", e);
@@ -143,7 +145,7 @@ const InstanceGraphicConsole: FC<Props> = ({
 
   useEffect(() => {
     notify.clear();
-    const websocketPromise = openVgaConsole();
+    const websocketPromise = openVgaConsole(force);
     return () => {
       try {
         window.spice_connection?.stop();