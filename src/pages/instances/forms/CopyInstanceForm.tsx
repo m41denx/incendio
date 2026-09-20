zabbly/incus-ui-canonical

Improve instance copy handling
- Pass the instance config map with volatile keys filtered out
- Preserve overrides for the root disk when copying
- Pass profile information to the copied instance

Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>
License: GPL-3.0-only (without Canonical CLA)

src/pages/instances/forms/CopyInstanceForm.tsx
@@ -14,7 +14,10 @@ import {
 import * as Yup from "yup";
 import { createInstance } from "api/instances";
 import { useNavigate } from "react-router-dom";
-import { instanceNameValidation } from "util/instances";
+import {
+  instanceIncludeConfigWhenCopying,
+  instanceNameValidation,
+} from "util/instances";
 import type { LxdDiskDevice } from "types/device";
 import { useEventQueue } from "context/eventQueue";
 import ClusterMemberSelector from "pages/cluster/ClusterMemberSelector";
@@ -28,6 +31,7 @@ import { useProjects } from "context/useProjects";
 import { useProjectEntitlements } from "util/entitlements/projects";
 import { useStoragePools } from "context/useStoragePools";
 import { useIsClustered } from "context/useIsClustered";
+import { isRootDisk } from "util/instanceValidation";
 
 interface Props {
   instance: LxdInstance;
@@ -106,11 +110,45 @@ const CopyInstanceForm: FC<Props> = ({ instance, close }) => {
     }),
     onSubmit: (values) => {
       const instanceLink = <InstanceLinkChip instance={instance} />;
+      const config = Object.fromEntries(
+        Object.entries(instance.config).filter(([key, _]) =>
+          instanceIncludeConfigWhenCopying(key),
+        ),
+      );
+
+      let rootDiskName = "";
+      const devices = Object.fromEntries(
+        Object.entries(instance.devices).map(([key, value]) => {
+          if (isRootDisk(value)) {
+            rootDiskName = key;
+            if (values.targetStoragePool !== "") {
+              return [key, { ...value, pool: values.targetStoragePool }];
+            }
+          }
+
+          return [key, value];
+        }),
+      );
+
+      if (rootDiskName === "" && values.targetStoragePool !== "") {
+        devices["root"] = {
+          path: "/",
+          type: "disk",
+          pool: values.targetStoragePool,
+        };
+      }
+
       createInstance(
         JSON.stringify({
           description: instance.description,
           name: values.instanceName,
           architecture: instance.architecture,
+          ephemeral: instance.ephemeral,
+          type: instance.type,
+          profiles: [...instance.profiles],
+          config: {
+            ...config,
+          },
           source: {
             allow_inconsistent: values.allowInconsistent,
             instance_only: values.instanceOnly,
@@ -119,12 +157,7 @@ const CopyInstanceForm: FC<Props> = ({ instance, close }) => {
             project: instance.project,
           },
           devices: {
-            ...instance.devices,
-            root: {
-              path: "/",
-              type: "disk",
-              pool: values.targetStoragePool,
-            },
+            ...devices,
           },
         }),
         values.targetProject,

m41denx/incendio

(cherry picked from commit f3b108edc076b83541c78ee9cc9d4a98a02267e4)

src/pages/instances/forms/CopyInstanceForm.tsx
@@ -13,8 +13,12 @@ import {
 import * as Yup from "yup";
 import { createInstance } from "api/instances";
 import { useNavigate } from "react-router-dom";
-import { instanceNameValidation } from "util/instances";
+import {
+  instanceIncludeConfigWhenCopying,
+  instanceNameValidation,
+} from "util/instances";
 import type { LxdDiskDevice } from "types/device";
+import type { FormDevice } from "types/formDevice";
 import { useEventQueue } from "context/eventQueue";
 import ClusterMemberSelector from "pages/cluster/ClusterMemberSelector";
 import { getUniqueResourceName, truncateEntityName } from "util/helpers";
@@ -27,6 +31,7 @@ import { useProjectEntitlements } from "util/entitlements/projects";
 import { useStoragePools } from "context/useStoragePools";
 import { useIsClustered } from "context/useIsClustered";
 import { InstanceRichChip } from "../InstanceRichChip";
+import { isRootDisk } from "util/devices";
 
 interface Props {
   instance: LxdInstance;
@@ -110,11 +115,45 @@ const CopyInstanceForm: FC<Props> = ({ instance, close }) => {
           projectName={instance.project}
         />
       );
+      const config = Object.fromEntries(
+        Object.entries(instance.config).filter(([key, _]) =>
+          instanceIncludeConfigWhenCopying(key),
+        ),
+      );
+
+      let rootDiskName = "";
+      const devices = Object.fromEntries(
+        Object.entries(instance.devices).map(([key, value]) => {
+          if (isRootDisk(value as FormDevice)) {
+            rootDiskName = key;
+            if (values.targetStoragePool !== "") {
+              return [key, { ...value, pool: values.targetStoragePool }];
+            }
+          }
+
+          return [key, value];
+        }),
+      );
+
+      if (rootDiskName === "" && values.targetStoragePool !== "") {
+        devices["root"] = {
+          path: "/",
+          type: "disk",
+          pool: values.targetStoragePool,
+        };
+      }
+
       createInstance(
         JSON.stringify({
           description: instance.description,
           name: values.instanceName,
           architecture: instance.architecture,
+          ephemeral: instance.ephemeral,
+          type: instance.type,
+          profiles: [...instance.profiles],
+          config: {
+            ...config,
+          },
           source: {
             allow_inconsistent: values.allowInconsistent,
             instance_only: values.instanceOnly,
@@ -123,12 +162,7 @@ const CopyInstanceForm: FC<Props> = ({ instance, close }) => {
             project: instance.project,
           },
           devices: {
-            ...instance.devices,
-            root: {
-              path: "/",
-              type: "disk",
-              pool: values.targetStoragePool,
-            },
+            ...devices,
           },
         }),
         values.targetProject,